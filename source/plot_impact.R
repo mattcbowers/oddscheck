a250c6ddede1f2668355ebb8bda52aa5fa2c9b3d
Matt Bowers <mcbowers@mba13.local>
2017-06-18 13:15:25 -0700

    Create script to make a plot showing impact.

---
+++ source/plot_impact.R
@@ -0,0 +1,13 @@
+library(tidyverse)
+tb <- tibble(donations = c(1, 1.37), type = c("without OddsCheck", "with OddsCheck"))
+theme_set(theme_gray(base_size = 18))
+
+tb %>%
+  mutate(type = reorder(type, donations)) %>% 
+  ggplot(aes(type, donations, fill = type)) +
+  labs(x = "", y = "") +
+  guides(fill = FALSE) +
+  theme( axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) +
+  geom_col()
+ggsave(file = "impact.png", path ="../figures/")
